d2f2c540ec04d9d7b5fff9419be42fff065f36aa
roggerchangg@gmail.com <roggerchangg@gmail.com@9b90dca3-ad31-8a47-3082-51995b6a5976>
2014-05-20 04:39:48 +0000

    Implementación de la funcionalidad de la pantalla RegistrarEmpresa

---
+++ trunk/MovilBolsaTrabajo/web/Scripts/js/config.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+//$("#navegacion").load("pages/general/navegacion.html");
+
+$("#footer").load("pages/general/footer.html");
